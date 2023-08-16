9f9068b5bce3c5732f06c2d02f45a43771019ac9
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-08-16 15:44:32 -0300

    Atualização Exercício 3

--- Exercicio3.java
+++ Exercicio3.java
@@ -8,27 +8,27 @@ public class Exercicio3 {
 		
 		String nome;
 		int idade;
-		boolean apto = false;
+		boolean apte = false;
 		
 		Scanner leia = new Scanner(System.in);
 		
-		System.out.println("Digite o nome do doador: ");
+		System.out.println("Digite o nome da pessoa que vai realizar a doação: ");
 		nome = leia.nextLine();
 		
-		System.out.println("Digite a idade do doador: ");
+		System.out.println("Digite a idade da pessoa que vai realizar a doação: ");
 		idade = leia.nextInt();
 		
 		System.out.println("É sua primeira doação de sangue na vida? ");
-		apto = leia.nextBoolean();
+		apte = leia.nextBoolean();
 		
 		if (idade >= 18 && idade <= 69)
-			if (idade >= 60 && idade <= 69 && apto == true) {
-                System.out.println(nome+" **não está** apto para doar sangue!");
+			if (idade >= 60 && idade <= 69 && apte == true) {
+                System.out.println(nome+" **não está** apte para doar sangue!");
             } else {
-                System.out.println(nome+" **está** apto para doar sangue!");
+                System.out.println(nome+" **está** apte para doar sangue!");
             }
         else {
-            System.out.println(nome+" **não está** apto para doar sangue!");
+            System.out.println(nome+" **não está** apte para doar sangue!");
         }
 	}
 }
